26cf73a53f862070ca8f002bdf8f09859f5feb57
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-07-05 13:40:36 -0400

    creating cookie stores

---
+++ main.js
@@ -0,0 +1,13 @@
+let levainBakery = {
+    name: Levain Bakery,
+    location: "New York, NY",
+    price: 4,
+    rating: "5/5"
+}
+
+let tiffsTreats = {
+    name: "Tiff's Treats",
+    location: "Austin, TX",
+    price: 2,
+    rating: "3/5"
+}
