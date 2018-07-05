bfbb05186e6cf98461e3259eaa030a4044d4b743
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-07-05 18:40:36 -0400

    added last store and added properties

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 let levainBakery = {
     name: Levain Bakery,
     location: "New York, NY",
-    price: 4,
+    cookieType: "chocolate chip walnut",
+    currentPrice: 4,
     rating: "5/5"
+    annualPriceIncrease = function (currentPrice) {
+        currentPrice = currentPrice * 1.0175;  
+        return currentPrice;
+    }
+    futurePrice = function (numOfYearsFromNow) {
+        return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    }
 }
 
 let tiffsTreats = {
     name: "Tiff's Treats",
     location: "Austin, TX",
-    price: 2,
+    cookieType: "chocolate chip"
+    currentPrice: 2,
     rating: "3/5"
+    annualPriceIncrease = function (currentPrice) {
+        currentPrice = currentPrice * 1.0175;  
+        return currentPrice;
+    }
+    futurePrice = function (numOfYearsFromNow) {
+        return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    }
+}
+
+let milkBar = {
+    name: "Milk Bar",
+    location: "New York, NY",
+    cookieType: "compost cookie"
+    currentPrice: 3.50,
+    rating: "4/5"
+    annualPriceIncrease = function (currentPrice) {
+        currentPrice = currentPrice * 1.0175;  
+        return currentPrice;
+    }
+    futurePrice = function (numOfYearsFromNow) {
+        return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    }
 }
