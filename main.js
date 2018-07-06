1ec314bf22bf0bef1cb3ecb6b130b7f2a1349ae8
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-07-05 20:07:48 -0400

    correcting code to reflect assignment reqs

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ let levainBakery = {
     cookieType: "chocolate chip walnut",
     currentPrice: 4,
     rating: "5/5",
+    minCustomersPerHour: 20,
+    maxCustomersPerHour: 200,
+    averageCookiesPerCustomer: 2.5,
     annualPriceIncrease: function (currentPrice) {
         currentPrice = currentPrice * 1.0175;  
         return currentPrice;
     },
     futurePrice: function (numOfYearsFromNow) {
         return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    },
+    customersPerHour: function () {
+        return Math.floor(20 + (Math.Random * 180));
     }
 }
 
@@ -19,12 +25,18 @@ let tiffsTreats = {
     cookieType: "chocolate chip",
     currentPrice: 2,
     rating: "3/5",
+    minCustomersPerHour: 10,
+    maxCustomersPerHour: 120,
+    averageCookiesPerCustomer: 5,
     annualPriceIncrease: function (currentPrice) {
         currentPrice = currentPrice * 1.0175;  
         return currentPrice;
     },
     futurePrice: function (numOfYearsFromNow) {
         return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    },
+    customersPerHour: function () {
+        return Math.floor(10 + (Math.Random * 110));
     }
 }
 
@@ -34,11 +46,17 @@ let milkBar = {
     cookieType: "compost cookie",
     currentPrice: 3.50,
     rating: "4/5",
+    minCustomersPerHour: 15,
+    maxCustomersPerHour: 150,
+    averageCookiesPerCustomer: 2,
     annualPriceIncrease: function (currentPrice) {
         currentPrice = currentPrice * 1.0175;  
         return currentPrice;
     },
     futurePrice: function (numOfYearsFromNow) {
         return currentPrice * (1 + (.0175 * numOfYearsFromNow));
+    },
+    customersPerHour: function () {
+        return Math.floor(15 + (Math.Random * 135));
     }
 }
