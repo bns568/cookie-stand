d8136291632d4b7c16745f515a8ed5c193f8e262
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-07-05 21:34:29 -0400

    finished Levain

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let levainBakery = {
     maxCustomersPerHour: 200,
     levainCustomers: [],
     cookiesSoldToday: 0,
-    averageCookiesPerCustomer: 2.5,
+    averageCookiesPerCustomer: 3,
     annualPriceIncrease: function () {
         this.currentPrice = this.currentPrice * 1.0175;  
         return this.currentPrice;
@@ -17,16 +17,22 @@ let levainBakery = {
         return this.currentPrice * (1 + (.0175 * numOfYearsFromNow));
     },
     customersPerHour: function () {
-        return Math.floor(20 + (Math.Random * 180));
+        let answer = Math.floor(20 + (Math.random() * 180));
+        //console.log ('Math random: ' + answer);
+        return answer; 
     },
     customerLog: function() {
         for (let i =0; i < 15; i++){
-            this.levainCustomers[i] = this.customersPerHour * this.customersPerHour;
+            //console.log(this.customersPerHour() + ' ' + this.averageCookiesPerCustomer);
+            this.levainCustomers[i] = this.customersPerHour() * this.averageCookiesPerCustomer;
+            //console.log( this.levainCustomers[i]);
         }
     },
     cookieDailySalesFunction: function() {
         for (let i = 0; i < this.levainCustomers.length; i++) {
-            this.cookiesSoldToday = this.cookiesSoldToday + this.levainCustomers[i]; 
+            console.log('customers:' + this.levainCustomers[i]);
+            this.cookiesSoldToday += this.levainCustomers[i]; 
+            console.log('cookies sold today: ' + this.cookiesSoldToday);
         }
     }
 };
@@ -54,7 +60,7 @@ let tiffsTreats = {
     },
     customerLog: function() {
         for (let i =0; i < 15; i++){
-            this.tiffsTreatsCustomers[i] = this.customersPerHour * this.customersPerHour;
+            this.tiffsTreatsCustomers[i] = this.customersPerHour * this.averageCookiesPerCustomer;
         }
     },
     cookieDailySalesFunction: function() {
@@ -87,7 +93,7 @@ let milkBar = {
     },
     customerLog: function() {
         for (let i =0; i < 15; i++){
-            this.milkBarCustomers[i] = this.customersPerHour * this.customersPerHour;
+            this.milkBarCustomers[i] = this.customersPerHour * this.averageCookiesPerCustomer;
         }
     },
     cookieDailySalesFunction: function() {
@@ -98,5 +104,32 @@ let milkBar = {
 };
 
 document.getElementById('levainBakeryName').innerText = levainBakery.name;
+levainBakery.customerLog();
+console.log('Customer Log: ' + levainBakery.levainCustomers);
+console.log('Customers per hour: ' + levainBakery.customersPerHour());
+console.log('Average cookies per customer: ' + levainBakery.averageCookiesPerCustomer);
+document.getElementById('levain6am').innerText = levainBakery.levainCustomers[0];
+document.getElementById('levain7am').innerText = levainBakery.levainCustomers[1];
+document.getElementById('levain8am').innerText = levainBakery.levainCustomers[2];
+document.getElementById('levain9am').innerText = levainBakery.levainCustomers[3];
+document.getElementById('levain10am').innerText = levainBakery.levainCustomers[4];
+document.getElementById('levain11am').innerText = levainBakery.levainCustomers[5];
+document.getElementById('levain12pm').innerText = levainBakery.levainCustomers[6];
+document.getElementById('levain1pm').innerText = levainBakery.levainCustomers[7];
+document.getElementById('levain2pm').innerText = levainBakery.levainCustomers[8];
+document.getElementById('levain3pm').innerText = levainBakery.levainCustomers[9];
+document.getElementById('levain4pm').innerText = levainBakery.levainCustomers[10];
+document.getElementById('levain5pm').innerText = levainBakery.levainCustomers[11];
+document.getElementById('levain6pm').innerText = levainBakery.levainCustomers[12];
+document.getElementById('levain7pm').innerText = levainBakery.levainCustomers[13];
+document.getElementById('levain8pm').innerText = levainBakery.levainCustomers[14];
+levainBakery.cookieDailySalesFunction();
+console.log(levainBakery.cookiesSoldToday);
+document.getElementById('levainBakeryTotal').innerText = levainBakery.cookiesSoldToday;
+
+
 document.getElementById('milkBarName').innerText = milkBar.name;
+milkBar.cookieDailySalesFunction;
+
 document.getElementById('tiffsTreatsName').innerText = tiffsTreats.name;
+tiffsTreats.cookieDailySalesFunction;
